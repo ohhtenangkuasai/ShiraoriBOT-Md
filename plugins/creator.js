70ebec0e64a2421b44cb5fc6e871a6b385bd40b5
ohhtenangkuasai <97328929+ohhtenangkuasai@users.noreply.github.com>
2022-01-25 21:55:43 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ async function handler(m) {
                     + 'VERSION:3.0\n' 
                     + 'N:;𝑀𝑒𝑙𝑙 💫;;;'
                     + 'FN:imell\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'ORG:imellOwner-BOTツ;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6285775238312:+62 857-7523-8312\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: '𝑀𝑒𝑙𝑙 💫', contacts: [{ vcard }] } }, { quoted: m })
